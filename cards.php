ebdc2b09d645f2cdf44eb2d557a86bc5e40d54fa
Micaela Mingrone <micacp09@gmail.com>
2023-02-19 15:03:36 -0300

    actualizando

---
+++ cards.php
@@ -0,0 +1,100 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Micaela Mingrone | Portfolio </title>
+    <!--CDN FONTAWESOME-->
+    <script src="https://kit.fontawesome.com/3ee2d30d4e.js" crossorigin="anonymous"></script>
+    <!--CDN ANIMATECSS-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+    <!--CDN BOOTSTRAP-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <!--CSS-->
+    <link href="css/estilos.css" rel="stylesheet">
+    <link href="css/cards.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div class="team-grid">
+        <div class="container">
+            <div class="intro">
+                <h2 class="text-center">Meet Our Team</h2>
+                <p class="text-center">A Bootstrap 4 example layout with an Meet our team section and team members</p>
+            </div>
+            <div class="row people d-flex justify-content-center">
+                <div class="col-md-4 col-lg-3 item">
+                    <div class="box" style="background-image:url(../portfolio/img/card-1.png); background-repeat:no-repeat;">
+                        <div class="cover">
+                            <h3 class="name">Página Web + panel de administrador</h3>
+                            <p class="title-card">Realizado con Bootstrap y PHP</p>
+                        </div>
+                    </div>
+                    <form class="d-flex justify-content-center">
+                        <a class="btn btn-styled btn-sm btn mb-3" href="#">Ver +</a>
+                    </form>
+                </div>
+                <div class="col-md-4 col-lg-3 item">
+                    <div class="box" style="background-image:url(../portfolio/img/card-2.png)">
+                        <div class="cover">
+                            <h3 class="name">Piedra, papel o tijera</h3>
+                            <p class="title-card">Juego que realicé para practicar Javascript vainilla</p>
+                         </div>
+                    </div>
+                    <form class="d-flex justify-content-center">
+                        <a class="btn btn-styled btn-sm btn mb-3" href="#">Ver +</a>
+                    </form>
+                </div>
+                <div class="col-md-4 col-lg-3 item">
+                    <div class="box" style="background-image:url(../portfolio/img/card-4.png)">
+                        <div class="cover">
+                            <h3 class="name">ToDo List</h3>
+                            <p class="title-card">App de consola utilizando NodeJs y otros paquetes como Colors e Inquirer</p>
+                        </div>
+                    </div>
+                    <form class="d-flex justify-content-center">
+                        <a class="btn btn-styled btn-sm btn mb-3" href="#">Ver +</a>
+                    </form>
+                </div>
+               
+            </div>
+            <div class="row people d-flex justify-content-center">
+                <div class="col-md-4 col-lg-3 item">
+                    <div class="box" style="background-image:url(../portfolio/img/card-3.png); background-repeat:no-repeat;">
+                        <div class="cover">
+                            <h3 class="name">Up-Task - Gestor de proyectos</h3>
+                            <p class="title-card">Desarrollado con NodeJs utilizando MVC,utilizando herramientas como Pug, Sequelize, SweetAlert, entre otros.</p>
+                        </div>
+                    </div>
+                    <form class="d-flex justify-content-center">
+                        <a class="btn btn-styled btn-sm btn mb-3" href="#">Ver +</a>
+                    </form>
+                </div>
+                <div class="col-md-4 col-lg-3 item">
+                    <div class="box" style="background-image:url(../portfolio/img/card-5.png)">
+                        <div class="cover">
+                            <h3 class="name">Clima App</h3>
+                            <p class="title-card">Reversión de la app de consola,en javascript vainilla utilizando fetch para la obtención de datos. El front realizado con Bootstrap y Css</p>
+                         </div>
+                    </div>
+                    <form class="d-flex justify-content-center">
+                        <a class="btn btn-styled btn-sm btn mb-3" href="#">Ver +</a>
+                    </form>
+                </div>
+                <div class="col-md-4 col-lg-3 item">
+                    <div class="box" style="background-image:url(../portfolio/img/card-6.png)">
+                        <div class="cover">
+                            <h3 class="name">App del clima en consola</h3>
+                            <p class="title-card">App donde utilicé la API de Open weather y MapBox para obtener los datos. Realizado en NodeJs, utilizando los paquetes Colors e Inquierer</p>
+                        </div>
+                    </div>
+                    <form class="d-flex justify-content-center">
+                        <a class="btn btn-styled btn-sm btn mb-3" href="#">Ver +</a>
+                    </form>
+                </div>
+               
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+</body>
+</html>    
---
+++ index.php
@@ -0,0 +1,283 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Micaela Mingrone | Portfolio </title>
+    <!--CDN FONTAWESOME-->
+    <script src="https://kit.fontawesome.com/3ee2d30d4e.js" crossorigin="anonymous"></script>
+    <!--CDN ANIMATECSS-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+    <!--CDN BOOTSTRAP-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <!--CSS-->
+    <link href="css/estilos.css" rel="stylesheet">
+    <!--Scroll-->
+    <script src="https://unpkg.com/scrollreveal"></script>
+    </head>
+  <body>
+   <header class="header">
+        <!--NAVBAR-->
+        <nav class="navbar navbar-expand-md navbar-fixed-top mt-2 sticky-sm-top">
+            <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                             <a class="nav-link" aria-current="page" href="#">¿Quién soy?</a>
+                        </li>
+                        <li class="nav-item">
+                             <a class="nav-link" href="#">Habilidades & conocimientos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link">Mis proyectos</a>
+                        </li>
+                     </ul>
+                        <form class="d-flex">
+                            <button class="btn btn-style btn-micv" type="submit">Mi CV</button>
+                         </form>
+                 </div>
+            </div>
+         </nav> 
+    </header>
+<section class="wrapper">
+    <div class="container">
+             <img src="img/asd.svg" class="col-md-6 float-md-end mb-3 mb-sm-0 ms-md-3 img-fluid img-portada" alt="">
+             <div class="row">  
+                 <div class="col-sm-12 d-flex justify-content-center text-center">
+                     <p class="title">¡hola!</p>
+                 </div>
+                 
+                 <div class="col-sm-12 d-flex justify-content-center text-center">
+                     <p class="subtitle">Soy Micaela Mingrone</p>
+                 </div>
+                 
+                 <div class="col-sm-12 d-flex justify-content-center text-center">
+                     <p class="subtitle">Fullstack web developer</p>
+                 </div>
+                
+                 <div class="col-sm-12 d-flex justify-content-center text-center">
+                     <form class="d-flex">
+                         <button class="btn btn-style btn-lg btn-contactame" type="submit">Contactame</button>
+                     </form>
+                 </div>
+             </div>   
+        </div>   <!-- ROW -->
+    </div>
+
+</section>
+<!-- CONTAINER-->
+
+    <section class="principal" id="scroll-1">
+        <div class="container">
+            <h1 class="titulo_celular text-center">¿Quién soy?</h1>
+            <div class="row">
+                <div class="col-sm-12 col-md-6 mt-5">
+                    <img class="img-desk img-fluid mb-3 mb-md-0 img-perfil mx-auto" src="img/mick.svg" alt="imagen de micaela mingrone" style="
+                    height: 450px;">
+                </div>
+
+                <div class="descripcion col-sm-12 col-md-6 text-center">
+                        <p id="reveal">👋 Hola, mi nombre es Micaela. <br/> Tengo 36 años, soy de Buenos Aires, Argentina.<br/><br/>
+                        Me defino como muy curiosa, metódica y analítica.<br/>
+                        Disfruto de los desafíos, me gusta mucho estudiar,<br/> enseñar y aprender. <br/>
+                        Resposable y comprometida, comunicarme acertivamente y saber trabajar en equipo son algunas de mis cualidades.<br/>
+                        Idear, diseñar y programar aplicaciones web <br/>hoy es mi gran pasión.</p>
+
+                        <form class="d-flex justify-content-center">
+                            <a class="btn btn-style btn-lg btn" href="#" id="reveal">Más sobre mí</a>
+                        </form>
+                </div>
+            </div>
+            <h2 class="habilidades text-center mt-5">{Habilidades y conocimientos/}</h2>
+                <div class="descripcion col-sm-12 mt-5 text-center">
+                        <p id="reveal">Durante los últimos años, me especialicé en el uso de las siguientes tecnologías <br/>asistiendo a distintos cursos, bootcamps y en forma autodidácta</p>
+                </div>
+            <div class="row" id="scroll-cards">
+                <div class="col-xs-6 col-md-3 mt-3">
+                    <img src="img/html-5.png" class="img-fluid imagen-icono" alt="">
+                </div>
+                <div class="col-xs-6 col-md-3 mt-3">
+                    <img src="img/css-3.png" class="img-fluid imagen-icono" alt="">
+                </div>
+                <div class="col-xs-6 col-md-3 mt-3">
+                    <img src="img/js(1).png" class="img-fluid imagen-icono" alt="">
+                </div>
+                <div class="col-xs-6 col-md-3 mt-3">
+                    <img src="img/php(1).png" class="img-fluid imagen-icono" alt="">
+                </div>
+                <div class="col-xs-6 col-md-3">
+                    <img src="img/nodejs(1).png" class="img-fluid imagen-icono" alt="">
+                </div>
+                <div class="col-xs-6 col-md-3">
+                    <img src="img/physics.png" class="img-fluid imagen-icono" alt="">
+                </div>
+                <div class="col-xs-6 col-md-3">
+                    <img src="img/mysql(1).png" class="img-fluid imagen-icono" alt="">
+                </div>
+                <div class="col-xs-6 col-md-3">
+                    <img src="img/sql-server(1).png" class="img-fluid imagen-icono" alt="">
+                </div>
+                <div class="col-xs-6 col-md-3">
+                    <img src="img/bootstrap.png" class="img-fluid imagen-icono" alt="">
+                </div>
+                <div class="col-xs-6 col-md-3">
+                    <img src="img/xd.png" class="img-fluid imagen-icono" alt="">
+                </div>
+                <div class="col-xs-6 col-md-3">
+                    <img src="img/photoshop.png" class="img-fluid imagen-icono" alt="">
+                </div>
+                <div class="col-xs-6 col-md-3">
+                    <img src="img/github.png" class="img-fluid imagen-icono" alt="">
+                </div>
+            </div>
+
+        </div> <!-- CONTAINER-->     
+    </section>
+    <!-- Proyectos cards--> 
+    <section class="proyectos" id="scroll-2">
+        <div class="container">
+        <h2 class="habilidades text-center" id="reveal">{Mis proyectos/}</h2>
+                <div class="texto-habilidades col-sm-12 mt-5 text-center">
+                    <p id="reveal">A modo de realizar prácticas que me permitan integrar lo aprendido, estos son algunos de los proyectos que desarrollé</p>
+                </div>
+        
+        <div class="row people d-flex justify-content-center" id="scroll-cards">
+                <div class="col-md-4 col-lg-3 item">
+                    <div class="box" style="background-image:url(../portfolio/img/card-1.png); background-repeat:no-repeat;">
+                        <div class="cover">
+                            <h3 class="name">Página Web + panel de administrador</h3>
+                            <p class="title-card">Realizado con Bootstrap y PHP</p>
+                        </div>
+                    </div>
+                    <form class="d-flex justify-content-center">
+                        <a class="btn btn-styled btn-sm btn mb-3" href="https://github.com/micamingrone/crudphpcac22">Ver +</a>
+                    </form>
+                </div>
+                <div class="col-md-4 col-lg-3 item">
+                    <div class="box" style="background-image:url(../portfolio/img/card-2.png)">
+                        <div class="cover">
+                            <h3 class="name">Piedra, papel o tijera</h3>
+                            <p class="title-card">Juego que realicé para practicar <br/>Javascript vainilla</p>
+                         </div>
+                    </div>
+                    <form class="d-flex justify-content-center">
+                        <a class="btn btn-styled btn-sm btn mb-3" href="https://micamingrone.github.io/">Ver +</a>
+                    </form>
+                </div>
+                <div class="col-md-4 col-lg-3 item">
+                    <div class="box" style="background-image:url(../portfolio/img/card-4.png)">
+                        <div class="cover">
+                            <h3 class="name">ToDo List</h3>
+                            <p class="title-card">App de consola utilizando NodeJs y otros paquetes como Colors e Inquirer</p>
+                        </div>
+                    </div>
+                    <form class="d-flex justify-content-center">
+                        <a class="btn btn-styled btn-sm btn mb-3" href="https://github.com/micamingrone/AppConsola/tree/master">Ver +</a>
+                    </form>
+                </div>
+               
+            </div>
+            <div class="row people d-flex justify-content-center">
+                <div class="col-md-4 col-lg-3 item">
+                    <div class="box" style="background-image:url(../portfolio/img/card-3.png); background-repeat:no-repeat;">
+                        <div class="cover">
+                            <h3 class="name">Up-Task - Gestor de proyectos</h3>
+                            <p class="title-card">Desarrollado con NodeJs utilizando MVC,utilizando herramientas como Pug, Sequelize, SweetAlert, entre otros.</p>
+                        </div>
+                    </div>
+                    <form class="d-flex justify-content-center">
+                        <a class="btn btn-styled btn-sm btn mb-3" href="https://github.com/micamingrone/upTask-proyect">Ver +</a>
+                    </form>
+                </div>
+                <div class="col-md-4 col-lg-3 item">
+                    <div class="box" style="background-image:url(../portfolio/img/card-5.png)">
+                        <div class="cover">
+                            <h3 class="name">Clima App</h3>
+                            <p class="title-card">Reversión de la app de consola,en javascript vainilla utilizando fetch para la obtención de datos. El front realizado con Bootstrap y Css</p>
+                         </div>
+                    </div>
+                    <form class="d-flex justify-content-center">
+                        <a class="btn btn-styled btn-sm btn mb-3" href="https://weather-app-js-xi.vercel.app/">Ver +</a>
+                    </form>
+                </div>
+                <div class="col-md-4 col-lg-3 item">
+                    <div class="box" style="background-image:url(../portfolio/img/card-6.png)">
+                        <div class="cover">
+                            <h3 class="name">App del clima en consola</h3>
+                            <p class="title-card">App donde utilicé la API de Open weather y MapBox para obtener los datos. Realizado en NodeJs, utilizando los paquetes Colors e Inquierer</p>
+                        </div>
+                    </div>
+                    <form class="d-flex justify-content-center">
+                        <a class="btn btn-styled btn-sm btn mb-3" href="https://github.com/micamingrone/appClima">Ver +</a>
+                    </form>
+                </div>
+                
+        <!-- Formulario-->  
+        <h2 class="habilidades text-center">{Contactame/}</h2>
+            <div class="row justify-content-center">
+                <div class="card-contacto card col-xs-3 col-md-4 mb-4">
+                    <div class="card-body">
+                        <div class="formulario form">
+                            <div class="mb-3">
+                                <label for="exampleFormControlTextarea1" class="form-label">Nombre</label>    
+                                <input type="text" class="form-control" placeholder="Nombre" aria-label="First name">
+                            </div>
+                    
+                            <div class="mb-3">
+                                <label for="exampleFormControlInput1" class="form-label">Correo electrónico</label>
+                                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="usuario@example.com">
+                            </div>
+
+                            <div class="mb-3">
+                                <label for="exampleFormControlTextarea1" class="form-label">Asunto</label>
+                                <input type="text" class="form-control" placeholder="Asunto" aria-label="First name">
+                            </div>
+
+                            <div class="mb-3">
+                                <label for="exampleFormControlTextarea1" class="form-label">Mensaje</label>
+                                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                            </div>
+
+                            <div class="mb-3 text-center">
+                                <a class="btn btn-styled btn-sm mb-3" href="#">Enviar</a>
+                            </div>
+            
+                        </div>
+                    </div>  
+                </div>    
+            </div>
+    </section>
+    <!-- Footer--> 
+    <section class="footer">
+    <footer class="footer">
+        <div class="container">
+            <div class="row justify-content-center">
+                <p class="text-center pt-4">&copy;{Hecho con ❤ por Mica Mingrone/} </p>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-4 text-center mt-2">
+                    <a href="https://micacp09@gmail.com"><i class="fa-solid fa-envelope fa-2xl"></i></a>
+                </div>
+                <div class="col-4 text-center mt-2">
+                    <a href="https://www.linkedin.com/in/micamingrone/"><i class="fa-brands fa-linkedin fa-2xl"></i></a>
+                </div>
+                <div class="col-4 text-center mt-2">
+                    <a href="https://github.com/micamingrone"><i class="fa-brands fa-github fa-2xl"></i></a>
+                </div>
+                
+            </div>
+        </div>
+    </footer>
+</section>    
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <script>
+		ScrollReveal().reveal('#scroll-1', { delay: 200 , duration: 600, easing: 'ease-in', reset: true})
+		ScrollReveal().reveal('#scroll-2', { delay: 500 , duration: 800, easing: 'ease-in', reset: true})
+        ScrollReveal().reveal('#scroll-cards', { delay: 500 , interval: 300, duration: 800, easing: 'ease-in', reset: true})
+	</script>
+</body>
+</html>
